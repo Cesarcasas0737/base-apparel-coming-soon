c1f6dd3a4ec3b1fce84729cc16fa846840caafa2
cesar <cesarcasas0737@gmail.com>
2023-10-30 17:11:27 -0700

    Removed starting wrapper div

--- base-apparel-coming-soon/src/modules/InfoCard.jsx
+++ base-apparel-coming-soon/src/modules/InfoCard.jsx
@@ -2,44 +2,42 @@ import '../styles/styles.css'
 
 function InfoCard(){
     return(
-        <div>
-            <div className="card">
-                <div className="cardLeftContent">
-                    <div className="cardHeader">
-                        <div className="cardLogo">
-
-                        </div>
-                        <p>Base Apperal</p>
-                    </div>
-
-                    <div className="cardModalMobile">
+        <div className="card">
+            <div className="cardLeftContent">
+                <div className="cardHeader">
+                    <div className="cardLogo">
 
                     </div>
-
-                    <div className="cardChildContent">
-                        <h1>WE&rsquo;RE</h1>
-                        <h1>COMING SOON</h1>
-                        <p>
-                            Hello fellow shoppers!
-                            We&rsquo;re currently building our new fashion store.
-                            Add your email below to stay up-to-date
-                            with announcements and our launch deals
-                        </p>
-                        <div className="cardEmailWrapper">
-                            <div className="cardemail">
-                                <form>
-                                    <input placeholder="Email Address"/>
-                
-                                    <button></button>
-                                </form>
-                            </div>
-                            <div className="error"></div>
-                        </div>
-                    </div>
+                    <p>Base Apperal</p>
                 </div>
-                <div className="cardRightContent cardModalDesktop">
+
+                <div className="cardModalMobile">
 
                 </div>
+
+                <div className="cardChildContent">
+                    <h1>WE&rsquo;RE</h1>
+                    <h1>COMING SOON</h1>
+                    <p>
+                        Hello fellow shoppers!
+                        We&rsquo;re currently building our new fashion store.
+                        Add your email below to stay up-to-date
+                        with announcements and our launch deals
+                    </p>
+                    <div className="cardEmailWrapper">
+                        <div className="cardemail">
+                            <form>
+                                <input placeholder="Email Address"/>
+            
+                                <button></button>
+                            </form>
+                        </div>
+                        <div className="error"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="cardRightContent cardModalDesktop">
+
             </div>
         </div>
     )
